57d4aa3d6c4be03103969fe134fbba0f369a95e1
duskstone <jedgardromero@gmail.com>
2020-05-28 16:07:54 -0400

    Intial Commit

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user.nil?
+            render json: ['invalid credentials'], status: 401
+        else
+            login!(@user)
+            render 'api/users/show';
+        end
+    end
+    
+    
+    def destroy
+        logout!
+        render json: {}, status: 404
+    end
+end
